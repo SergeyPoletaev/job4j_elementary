636e5d0748b3da0652c8a9965b6fa4f536e1ee6a
SergeyPoletaev <info.elekit@yandex.ru>
2021-05-31 12:14:36 +0300

    4. Переопределение [#174890 #95290]

---
+++ src/ru/job4j/inheritance/JSONReport.java
@@ -0,0 +1,21 @@
+package ru.job4j.inheritance;
+
+public class JSONReport extends TextReport {
+
+    @Override
+    public String generate(String name, String body) {
+        return "{"
+                + System.lineSeparator()
+                + "    name : " + name + ","
+                + System.lineSeparator()
+                + "    body : " + body
+                + System.lineSeparator()
+                + "}";
+    }
+
+    public static void main(String[] args) {
+        JSONReport report = new JSONReport();
+        String text = report.generate("tuzik", "murka");
+        System.out.println(text);
+    }
+}
