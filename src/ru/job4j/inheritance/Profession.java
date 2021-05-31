8d392609a8e02b8f940802dd0a55bc7dd401f186
SergeyPoletaev <info.elekit@yandex.ru>
2021-05-31 11:43:50 +0300

    изменен пакет

--- src/ru/job4j/inheritance/Profession.java
+++ src/ru/job4j/inheritance/Profession.java
@@ -1,4 +1,4 @@
-package ru.job4j;
+package ru.job4j.inheritance;
 
 import java.util.Date;
 
--- src/ru/job4j/inheritance/Technologist.java
+++ src/ru/job4j/inheritance/Technologist.java
@@ -1,4 +1,4 @@
-package ru.job4j;
+package ru.job4j.inheritance;
 
 public class Technologist extends Engineer {
   private Double timeStart;
