fac1657939e54f1dae2b43099b53dc6e52a59e47
SergeyPoletaev <info.elekit@yandex.ru>
2021-06-30 13:13:58 +0300

    3. Интерфейс Input [#181092 #95245]

---
+++ src/ru/job4j/poly/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    @Override
+    public void drive() {
+        System.out.println("Автобус едет");
+    }
+
+    @Override
+    public void passengers(int count) {
+
+    }
+
+    @Override
+    public double fill(double fuel) {
+        return 0;
+    }
+}
