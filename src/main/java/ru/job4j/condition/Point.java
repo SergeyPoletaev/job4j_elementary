1dead1d22db0194814d29d3a41f0d3c3f267b911
SergeyPoletaev <info.elekit@yandex.ru>
2022-01-04 17:22:06 +0300

    3. Тесты для определения расстояния между точками. [#159459], откорректирована последовательность аргументов в методе Point.distance()

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -2,7 +2,7 @@ package ru.job4j.condition;
 
 public class Point {
 
-    public static double distance(int x1, int x2, int y1, int y2) {
+    public static double distance(int x1, int y1, int x2, int y2) {
         return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
     }
 
@@ -11,7 +11,7 @@ public class Point {
         int x2 = 100;
         int y1 = 0;
         int y2 = 112;
-        double result = Point.distance(x1, x2, y1, y2);
-        System.out.println("Result distance (" + x1 + ", " + x2 + ") to (" + y1 + ", " + y2 + ") = " + result);
+        double result = Point.distance(x1, y1, x2, y2);
+        System.out.println("Result distance (" + x1 + ", " + y1 + ") to (" + x2 + ", " + y2 + ") = " + result);
     }
 }
---
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,62 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PointTest {
+
+    @Test
+    public void when00to00ThenDist0() {
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 0;
+        int y2 = 0;
+        double expected = 0;
+        double rsl = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, rsl, 0.01);
+    }
+
+    @Test
+    public void when00to01ThenDist1() {
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 0;
+        int y2 = 1;
+        double expected = 1;
+        double rsl = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, rsl, 0.01);
+    }
+
+    @Test
+    public void when00to11ThenDist1Dot41() {
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 1;
+        int y2 = 1;
+        double expected = 1.41;
+        double rsl = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, rsl, 0.01);
+    }
+
+    @Test
+    public void when01to11ThenDist1() {
+        int x1 = 0;
+        int y1 = 1;
+        int x2 = 1;
+        int y2 = 1;
+        double expected = 1;
+        double rsl = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, rsl, 0.01);
+    }
+
+    @Test
+    public void when11to11ThenDist0() {
+        int x1 = 1;
+        int y1 = 1;
+        int x2 = 1;
+        int y2 = 1;
+        double expected = 0;
+        double rsl = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, rsl, 0.01);
+    }
+}
