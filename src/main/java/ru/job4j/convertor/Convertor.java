7a23982fe7bff2df4629c428da8f037149586af4
SergeyPoletaev <info.elekit@yandex.ru>
2022-01-04 13:10:04 +0300

    0. Что такое тестирование [#159431]. Добавлен тест на метод rubleToEuro()

--- src/main/java/ru/job4j/convertor/Convertor.java
+++ src/main/java/ru/job4j/convertor/Convertor.java
@@ -18,10 +18,16 @@ public class Convertor {
         System.out.println("125 rubles are " + euro + " euro");
         System.out.println("250 rubles are " + dollar + " dollars");
 
-        float in = 112F;
-        float expected = 1.8666667F;
-        float out = Convertor.rubleToDollar(in);
-        boolean passed = out == expected;
-        System.out.println("112 rubles are 1.8666667. Test result: " + passed);
+        float inDollConv = 112F;
+        float expectedDollConv = 1.8666667F;
+        float outDollConv = Convertor.rubleToDollar(inDollConv);
+        boolean passedDollConv = outDollConv == expectedDollConv;
+        System.out.println("112 rubles are 1.8666667. Test result: " + passedDollConv);
+
+        float inEuroConv = 140F;
+        float expectedEuroConv = 2F;
+        float outEuroConv = Convertor.rubleToEuro(inEuroConv);
+        boolean passedEuroConv = expectedEuroConv == outEuroConv;
+        System.out.println("140 rubles are 2. Test result : " + passedEuroConv);
     }
 }
