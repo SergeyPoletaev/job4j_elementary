e02c2081e3ba6d8aac57f1948ec4b4d1bec88718
SergeyPoletaev <info.elekit@yandex.ru>
2022-01-09 22:29:25 +0300

    5.7. Ипотека [#156315 #248647]. Исправления по замечанию.

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,10 +3,9 @@ package ru.job4j.loop;
 public class Mortgage {
 
     public static int year(double amount, double salary, double percent) {
-        double debt = amount;
         int year = 0;
-        while (debt > 0) {
-            debt = debt + (debt * percent) / 100 - salary;
+        while (amount > 0) {
+            amount += (amount * percent) / 100 - salary;
             year++;
         }
         return year;
