755c9e97f837966503acaf786c1cabf78695f0d2
SergeyPoletaev <info.elekit@yandex.ru>
2022-01-10 12:53:25 +0300

    6.0.1. Объявление массива. [#3613 #248854]. Исправления по замечанию.

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -6,17 +6,5 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
-        System.out.println("Размер массива ages = " + ages.length);
-        System.out.println("Размер массива surnames = " + surnames.length);
-        System.out.println("Размер массива prices = " + prices.length);
-
-        String[] names = new String[4];
-        names[0] = "Ivan";
-        names[1] = "Petr";
-        names[2] = "Anna";
-        names[3] = "Nik";
-        for (String name : names) {
-            System.out.println(name);
-        }
     }
 }
